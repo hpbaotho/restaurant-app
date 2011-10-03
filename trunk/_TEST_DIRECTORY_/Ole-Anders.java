5f8a3c04249b9ef3d8ba6979909b2d31e19fddf0
andershovl@gmail.com <andershovl@gmail.com@80ff7cb0-16be-5205-6515-ff77cb6e3d5b>
2011-10-03 11:24:28 +0000

    Cleaned the code

--- trunk/_TEST_DIRECTORY_/Ole-Anders.java
+++ trunk/_TEST_DIRECTORY_/Ole-Anders.java
@@ -2,10 +2,7 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello Anders");
         System.out.println("Another print");
-<<<<<<< .mine
         int[] numbers = {1, 2, 5, 14, 356};
-=======
-        System.out.println("suverent med tester")
->>>>>>> .r45
+        System.out.println("suverent med tester");
     }
 }
