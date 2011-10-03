6415f10b2ea026d8d9d4c677f9c1382f3b797ddd
ahmet91milan <ahmet91milan@80ff7cb0-16be-5205-6515-ff77cb6e3d5b>
2011-10-03 07:54:47 +0000

    ahmet+terje

---
+++ trunk/_TEST_DIRECTORY_/ahmet+terje/src/ahmet/terje/AhmetTerje.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ahmet.terje;
+
+/**
+ *
+ * @author Ahmet Cinar
+ */
+public class AhmetTerje {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello Gangsta");
+    }
+}
