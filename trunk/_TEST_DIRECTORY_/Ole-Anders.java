ef629916faef3743b1982fafdc9e08b16b790da2
andershovl@gmail.com <andershovl@gmail.com@80ff7cb0-16be-5205-6515-ff77cb6e3d5b>
2011-10-03 11:22:01 +0000

    added int[]

--- trunk/_TEST_DIRECTORY_/Ole-Anders.java
+++ trunk/_TEST_DIRECTORY_/Ole-Anders.java
@@ -2,6 +2,10 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello Anders");
         System.out.println("Another print");
+<<<<<<< .mine
+        int[] numbers = {1, 2, 5, 14, 356};
+=======
         System.out.println("suverent med tester")
+>>>>>>> .r45
     }
 }
