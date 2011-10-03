cb1631861f1ad0a3c523589b2434e4cc9ea1c3d4
ahmet91milan <ahmet91milan@80ff7cb0-16be-5205-6515-ff77cb6e3d5b>
2011-10-03 08:06:27 +0000

    ss

--- _TEST_DIRECTORY_/ahmet+terje/src/ahmet/terje/AhmetTerje.java
+++ _TEST_DIRECTORY_/ahmet+terje/src/ahmet/terje/AhmetTerje.java
@@ -14,6 +14,6 @@ public class AhmetTerje {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello gangsta");
+        System.out.println("Hello World");
     }
 }
