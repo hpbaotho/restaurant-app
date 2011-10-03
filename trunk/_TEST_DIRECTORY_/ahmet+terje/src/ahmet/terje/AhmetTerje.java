6086f84552ac6ea40ea75319f72b66ec6e605dd3
ahmet91milan <ahmet91milan@80ff7cb0-16be-5205-6515-ff77cb6e3d5b>
2011-10-03 08:08:16 +0000

    heisann

--- trunk/_TEST_DIRECTORY_/ahmet+terje/src/ahmet/terje/AhmetTerje.java
+++ trunk/_TEST_DIRECTORY_/ahmet+terje/src/ahmet/terje/AhmetTerje.java
@@ -14,6 +14,7 @@ public class AhmetTerje {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello students");
+        System.out.println("heisann");
     }
 }
