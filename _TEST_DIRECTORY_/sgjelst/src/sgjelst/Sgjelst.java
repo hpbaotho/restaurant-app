a02b967d8a22690382af102378d262114f592b09
sgjelst <sgjelst@gmail.com>
2011-10-03 07:59:39 +0000

    commitmod

--- _TEST_DIRECTORY_/sgjelst/src/sgjelst/Sgjelst.java
+++ _TEST_DIRECTORY_/sgjelst/src/sgjelst/Sgjelst.java
@@ -14,7 +14,7 @@ public class Sgjelst {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hellghuigruio World");
         System.err.println("whuuuuuuuuuut");
     }
 }
